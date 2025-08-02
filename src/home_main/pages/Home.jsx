489977b3d42c29ae839bc8e012778ac85b3163e4
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-02 22:31:29 +0530

    create  home , about, contact.projects,services compoennets🌟

---
+++ src/home_main/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-full bg-amber-700 flex">
+        <div className="w-[55%] bg-amber-300">
+          <h1></h1>
+        </div>
+
+        <div className="w-[45%] bg-amber-500"></div>
+      </div>
+    </>
+  );
+}
