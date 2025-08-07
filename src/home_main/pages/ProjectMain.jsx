efcb2702883663ca9198885b3fe583841b15262c
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-08-07 16:19:45 +0530

    Enhanced ProjectMain with active link highlighting using state-driven underline styling and responsive text sizing for sub-navigation, maintaining dark/light mode support 📂✨

--- src/home_main/pages/ProjectMain.jsx
+++ src/home_main/pages/ProjectMain.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Project from "../components/project/Project";
 import Cetification from "../components/project/Cetification";
@@ -7,6 +7,7 @@ import { DarkContext } from "../../utillls/context";
 
 export default function ProjectMain() {
   const { dark, setDark } = useContext(DarkContext);
+  const [selectButton, setSelectButton] = useState("");
   return (
     <>
       <div
@@ -16,9 +17,35 @@ export default function ProjectMain() {
       >
         <div className="h-full">
           <div className="h-[10%]">
-            <div className="flex items-center justify-start h-full font-bold gap-x-8 text-main_title text-accent">
-              <Link to="/projects/">Projects </Link>
-              <Link to="/projects/cetification">Cetification</Link>
+            <div className="flex items-center justify-start h-full font-bold gap-x-8 sm:text-main_title text-accent text-secondary_title">
+              <Link
+                to="/projects/"
+                className={`${selectButton == "project" && "underline "} ${
+                  dark != "false"
+                    ? "bg-primary decoration-primary_white"
+                    : "bg-primary_white  decoration-primary"
+                }  `}
+                onClick={() => {
+                  setSelectButton("project");
+                }}
+              >
+                Projects{" "}
+              </Link>
+              <Link
+                className={`${selectButton == "cetifications" && " underline "}
+                ${
+                  dark != "false"
+                    ? "bg-primary decoration-primary_white"
+                    : "bg-primary_white  decoration-primary"
+                }  }
+                `}
+                to="/projects/cetification"
+                onClick={() => {
+                  setSelectButton("cetifications");
+                }}
+              >
+                Cetification
+              </Link>
             </div>
           </div>
 
